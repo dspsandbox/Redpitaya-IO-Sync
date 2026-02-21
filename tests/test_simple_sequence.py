367ac22232454cf59afe5ae3be2f5571fcda1327
pau gomez <paugomezkabelka@gmail.com>
2026-02-21 01:57:44 +0100

    clean up example

--- tests/test_simple_sequence.py
+++ tests/test_simple_sequence.py
@@ -4,60 +4,25 @@ from redpitaya_io_sync.frame import IoSyncFrame, TriggerSource
 
 from pprint import pprint
 
-
+#Device
 rp = Rp_125_14_Mock(ip="192.168.1.143", label="my_rp_125_14_mock")
-seq = IoSequence()
+
+#Frame
 fr = IoSyncFrame(device=rp, trig=TriggerSource.TrigImmediate, timeout=None)
 fr.reset()
-
 fr.analog_out_1.frequency(1e6)
 fr.digital_io_1.tristate(val = 0, mask=0xffff)
-
-
-# fr.analog_out_1.phase(90)
 fr.analog_in_1.frequency(100e3)
-# fr.analog_out_1.amplitude(t, val, update)
-# fr.analog_out_2.amplitude(t, val, update)
-# fr.analog_out_1.reset_phase(t, val, update)
-# fr.analog_out_2.reset_phase(t, val, update)
-
-# fr.analog_in_1.frequency(t, val, update)
-# fr.analog_in_2.frequency(t, val, update)
-# fr.analog_in_1.phase(t, val, update)
-# fr.analog_in_2.phase(t, val, update)
-# fr.analog_in_1.amplitude(t, val, update)
-# fr.analog_in_2.amplitude(t, val, update)
-# fr.analog_in_1.reset_phase(t, val, update)
-# fr.analog_in_2.reset_phase(t, val, update)
-# fr.analog_in_1.acquire(t, samples, dec, label)
-# fr.analog_in_2.acquire(t, samples, dec, label)
-
 fr.wait(0x1000000)
 fr.pwm_1.duty_cycle(0.5)
 
+#Sequence
+seq = IoSequence()
 for i in range(10):
     seq.add_frame(fr, label=f"my_frame_{i}")
-
 seq.upload()
 seq.start()
+
+#Print status
 pprint(seq.get_status())
 
-# fr.xadc.acquire(t, samples, dec, label)
-
-# fr.digital.output(t, mask, val)
-# fr.digital.tristate(t, mask, val)
-# fr.digital.acquire(t, samples, dec, label)
-
-# fr.monitor.source(t, val)
-# fr.monitor.acquire(t, samples, dec, label)
-
-
-# seq.reset()
-# seq.append(fr, label)
-# seq.replace(label, fr)
-
-# rp.load(seq)
-# rp.start()
-# rp.stop()
-# rp.get_status() 
-# rp.get_data()
